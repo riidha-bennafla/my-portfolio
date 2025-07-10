acf0443e40ec5d3b27c21cb1401fd53aa158ca2d
Ridha <bennafla.riidha@gmail.com>
2025-07-10 17:12:22 +0100

    Add a data/index.tsx file and Assets

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,21 @@
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "../data";
+
 const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
